78da53fed010b05cbc91891c57aed98836651902
aisven <aisven822@gmail.com>
2023-06-14 12:49:35 +0200

    add infra to generate slides

---
+++ slides/content/slides.md
@@ -0,0 +1,28 @@
+---
+bibliography: "bibliography.bib"
+link-citations: true
+urlcolor: "blue"
+---
+
+### Total Variation in theory
+
+$$ 𝖳𝖵(\mathbb{P}_θ,\mathbb{P}_{θ\prime}) = \max_{A \subset E}|\mathbb{P}_θ(A) - \mathbb{P}_{θ\prime}(A)| $$
+
+### Total Variation of continuous probability distributions
+
+$$ 𝖳𝖵(\mathbb{P}_θ,\mathbb{P}_{θ\prime}) = \frac{1}{2} \int_E|f_θ(x) - f_{θ\prime}(x)|\,dx $$
+
+### Kullback-Leibler divergence aka. relative entropy
+
+$$ 𝖪𝖫(\mathbb{P}_θ,\mathbb{P}_{θ\prime}) =  \int_Ef_θ(x)\log\left(\frac{f_θ(x)}{f_{θ\prime}(x)}\right)\,dx $$
+
+- not symmetric (compare to variants of the Jensen–Shannon divergence)
+- greater or equal to zero
+- definite, i.e. one minimum with $𝖪𝖫(\mathbb{P}_θ,\mathbb{P}_{θ\prime}) = 0$
+- not a distance
+- a divergence
+- can be statistically estimated (by the law of large numbers via an average)
+
+# KL-divergence between two Gaussian distributions
+
+TODO
---
+++ slides/generate_slides.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+CONTENTDIR="content"
+BUILDDIR="target"
+FILENAME="slides"
+ASSETSDIR="assets"
+
+download_csl() {
+    wget -O "${ASSETSDIR}/harvard-anglia-ruskin-university.csl" \
+        "https://raw.githubusercontent.com/citation-style-language/styles/master/harvard-anglia-ruskin-university.csl"
+}
+
+pdf_print() {
+    mkdir -p "${BUILDDIR}"
+    echo "Creating pdf-print output"
+    pandoc "${CONTENTDIR}/${FILENAME}.md" \
+        --resource-path="${CONTENTDIR}" \
+        --citeproc \
+        --csl="${ASSETSDIR}/harvard-anglia-ruskin-university-custom.csl" \
+        --from="markdown+tex_math_single_backslash+tex_math_dollars+raw_tex" \
+        --to="latex" \
+        --output="${BUILDDIR}/output_print.pdf" \
+        --pdf-engine="xelatex" \
+        --include-in-header="layouts/print.tex"
+}
+
+pdf_ereader() {
+    mkdir -p "${BUILDDIR}"
+    echo "Creating pdf-ereader output"
+    pandoc "${CONTENTDIR}/${FILENAME}.md" \
+        --resource-path="${CONTENTDIR}" \
+        --citeproc \
+        --csl="${ASSETSDIR}/harvard-anglia-ruskin-university-custom.csl" \
+        --from="markdown+tex_math_single_backslash+tex_math_dollars+raw_tex" \
+        --to="latex" \
+        --output="${BUILDDIR}/output_ereader.pdf" \
+        --pdf-engine="xelatex" \
+        --include-in-header="layouts/ereader.tex"
+}
+
+# Allows to call a function based on arguments passed to the script
+# Example: `./generate_slides.sh pdf_print`
+$*
