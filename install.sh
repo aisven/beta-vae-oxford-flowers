34fb50d3dd9d8ef10c2351c40928652afb1d171d
aisven <aisven822@gmail.com>
2025-11-20 17:07:21 +0100

    improve comments

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-# pip install .
-# also install dev and test dependencies
+# install project with dependencies plus dev and test dependencies
 pip install .[dev,test]
 
 # remove the project itself to resemble a --no-root or --no-project behavior
